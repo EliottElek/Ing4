ffc77b6af945ed1cd26550fe6dcf6d2d49aef5f1
EliottElek <64375473+EliottElek@users.noreply.github.com>
2021-09-15 10:29:28 +0200

    q3

--- OperatingSystems/lab1/Question3.c
+++ OperatingSystems/lab1/Question3.c
@@ -1,3 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<wait.h>
+
+int main(int argc, char *argv[])
+{
 int i = 5;
 if (fork() == 0) {
 // I’m the child
@@ -6,4 +14,6 @@ i++;
 // I’m the parent
 sleep(3); // sleep for 3 seconds
 printf("%d\n", i); // what happens here ?? Explain
+}
+  return 0; 
 }
