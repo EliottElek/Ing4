9f0338a6db652587a537ac7c8b81d34c6dc349c0
EliottElek <64375473+EliottElek@users.noreply.github.com>
2021-09-15 12:18:16 +0200

    fix: Q5

--- OperatingSystems/lab1/Question5.c
+++ OperatingSystems/lab1/Question5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]){
+int main(){
     printf("Current process's ID : %d\n", getpid());
     char* args[] = {"firefox", "NULL"};
     execvp(args[0], args);
