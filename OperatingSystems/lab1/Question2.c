b41dec0a9e14623c11c76289ac62f1165cbb0c1a
EliottElek <64375473+EliottElek@users.noreply.github.com>
2021-09-15 10:08:21 +0200

    Question2.c

--- OperatingSystems/lab1/Question2.c
+++ OperatingSystems/lab1/Question2.c
@@ -4,35 +4,14 @@
 #include<unistd.h>
 #include<wait.h>
 
-// https://stackoverflow.com/questions/46368813/fork-returns-0-but-the-child-process-getpid-0-why
-
 int main(int argc, char *argv[])
 {
-
-    printf("I am: %d\n", (int)getpid());
-    printf("Parent is: %d\n", (int)getppid());
-
-
-    pid_t pid = fork();
-
-    printf("fork returned: %d\n", (int)pid);
-
-    if (pid < 0)
-    {
-        perror("fork failed");  
-    }
-
-    if (pid==0)
-    {
-        printf("I am the child with pid %d\n", (int)getpid());
-        sleep(5);
-        printf("Child exiting...\n");
-        exit(0);
-    }
-
-    printf("I am the parent with pid %d, waiting for the child\n", (int)getpid());
-    wait(NULL);
-    printf("Parent ending. \n");
-
-    return 0;
+   if (fork() == 0){
+       printf("I'm the child process ! \n");
+   }else {
+       printf("I'm the parent process ! \n");
+   }
+   printf("My ID : ", getpid());
+   printf("My parent ID : ", getppid()); 
+   return 0; 
 }
