61ce6ae82941e5c2f6f3a86302aa5269bf0be66c
EliottElek <64375473+EliottElek@users.noreply.github.com>
2021-09-15 18:28:42 +0200

    fix: removed brackets around NULL

--- OperatingSystems/lab1/Question5.c
+++ OperatingSystems/lab1/Question5.c
@@ -3,7 +3,7 @@
 
 int main(){
     printf("Current process's ID : %d\n", getpid());
-    char* args[] = {"firefox", "NULL"};
+    char* args[] = {"firefox", NULL};
      int a = execvp("firefox", args);
     fprintf(stdout, "execvp() returned %d\n", a);
     return 0;
