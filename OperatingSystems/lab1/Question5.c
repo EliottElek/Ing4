946a270ee1f11d2cf55ba4bd931a065232d45e57
EliottElek <64375473+EliottElek@users.noreply.github.com>
2021-09-15 18:22:21 +0200

    fix: q5

--- OperatingSystems/lab1/Question5.c
+++ OperatingSystems/lab1/Question5.c
@@ -4,7 +4,7 @@
 int main(){
     printf("Current process's ID : %d\n", getpid());
     char* args[] = {"firefox", "NULL"};
-     int a = execvp("/usr/bin/firefox", args);
+     int a = execvp("firefox", args);
     fprintf(stdout, "execvp() returned %d\n", a);
     return 0;
 }
