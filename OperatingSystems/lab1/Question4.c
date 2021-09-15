d2358fd00c99df642c2a8f5345b2403fe2aec3e0
EliottElek <64375473+EliottElek@users.noreply.github.com>
2021-09-15 10:42:08 +0200

    q4

---
+++ OperatingSystems/lab1/Question4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<wait.h>
+
+int main(int argc, char *argv[])
+{
+ if (fork() == 0) {
+       printf("I'm a child (Generation 1) ! \n");
+  if (fork() == 0) {
+       printf("I'm a child (Generation 2) ! \n");
+  }
+ } else {
+       printf("I'm a parent (Generation 1) ! \n");
+ if (fork() == 0) {
+       printf("I'm a child (Generation 1) ! \n");
+ }
+ sleep(3);
+  return 0; 
+}
