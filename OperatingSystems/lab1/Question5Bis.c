d2d7e4e36df3726448aae8859e1751f687b1e21e
EliottElek <64375473+EliottElek@users.noreply.github.com>
2021-09-17 14:38:17 +0200

    feat: Q5BIS

--- OperatingSystems/lab1/Question5Bis.c
+++ OperatingSystems/lab1/Question5Bis.c
@@ -1,14 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 int main(){
-
-
-    // display the process id
     printf("\nThis is the ID : %d\n",getpid());
-
-    // simply use any exec call !
     char cmd[30];
     strcpy(cmd,"Firefox");
     execl("/bin/sh","sh","-c",cmd, (char) NULL);
-
-
     return 0;
 }
